079c93887a213cc547a76fe6a8aebbf5f990a137
Christopher Roberson <chris@objectink.com>
2014-08-16 23:17:24 -0500

    Cleaned up JS

--- ng-titlecase.js
+++ ng-titlecase.js
@@ -5,7 +5,7 @@ angular.module('ng').filter('titlecase', function() {
             return '';
         var words = input.split(' ');
         for (var i = 0; i < words.length; i++) {
-            words[i] = words[i].toLowerCase(); // lowercase everything to get rid of weird casing issues
+            words[i] = words[i].toLowerCase(); 
             words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
         }
         return words.join(' ');
